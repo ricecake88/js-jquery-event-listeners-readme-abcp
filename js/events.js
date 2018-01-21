b77eba018176aa46e0e1ab2eb9d192a962afcf1a
Grace Shih <vocalise@gmail.com>
2018-01-21 18:14:12 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,34 @@
 $(document).ready(function(){
 
 // call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
+
+function getIt() {
+  $('p').on('click', alert("Hey!"));
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $("img").addClass('tasty')
+  });
+  
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(key) {
+    if (key.which == 71) {
+      alert('You have pressed the G Key!');
+    }
+  })
+}
+
+function submitIt() {
+  $("form").on('submit', function() {
+      alert("Your form is going to be submitted now.");
+  })
+  
+}

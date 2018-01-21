9c8ff6b19d62d2a7384fbc13bc8f8371a82816f1
Grace Shih <vocalise@gmail.com>
2018-01-21 18:18:21 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -4,9 +4,12 @@ $(document).ready(function(){
 
 // call functions here
   getIt();
+<<<<<<< HEAD
   frameIt();
   pressIt();
   submitIt();
+=======
+>>>>>>> fde85bd2e94af1c1b98cd0d363b59db0637786df
 });
 
 function getIt() {
@@ -14,14 +17,22 @@ function getIt() {
 }
 
 function frameIt() {
+<<<<<<< HEAD
   $('img').on('load', function() {
+=======
+  $('document').on('load', function() {
+>>>>>>> fde85bd2e94af1c1b98cd0d363b59db0637786df
     $("img").addClass('tasty')
   });
   
 }
 
 function pressIt() {
+<<<<<<< HEAD
   $('#typing').on('keydown', function(key) {
+=======
+  $('document').on('keydown', function(key) {
+>>>>>>> fde85bd2e94af1c1b98cd0d363b59db0637786df
     if (key.which == 71) {
       alert('You have pressed the G Key!');
     }
@@ -29,8 +40,15 @@ function pressIt() {
 }
 
 function submitIt() {
+<<<<<<< HEAD
   $("form").on('submit', function() {
       alert("Your form is going to be submitted now.");
+=======
+  $('document').on('submit', function() {
+    if ($("#typing").val()) {
+      alert("Your form is going to be submitted now.");
+    }
+>>>>>>> fde85bd2e94af1c1b98cd0d363b59db0637786df
   })
   
 }
